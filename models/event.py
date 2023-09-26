be23c3d45881d54519864e6cb67e80bce258032f
DutsAndrew <dutsandrew@gmail.com>
2023-09-25 18:14:37 -0600

    event model updates

--- models/event.py
+++ models/event.py
@@ -2,6 +2,7 @@ from pydantic import BaseModel, Field
 from models.bson_object_id import PyObjectId
 from bson import ObjectId
 from typing import Optional
+from datetime import datetime
 
 # Events should have the following:
 
@@ -14,7 +15,12 @@ class Event(BaseModel):
     id: PyObjectId = Field(default_factory=PyObjectId, alias="_id")
     calendar: Optional[PyObjectId] = Field(None)
     created_by: Optional[PyObjectId] = Field(None)
+    event_date_and_time: datetime = Field(default_factory=datetime.now, required=True)
+    event_description: str = Field(default_factory=str)
+    event_name: str = Field(default_factory=str, required=True)
     patterns: Optional[str] = Field(None)
+    repeats: bool = Field(default_factory=False)
+
 
     model_config= {
         "populate_by_name": True,
