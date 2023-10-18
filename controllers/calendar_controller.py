24b8992fd5f8acd34a1a6da96312e65c39acaec1
DutsAndrew <dutsandrew@gmail.com>
2023-10-17 20:25:01 -0600

    added calendar route to allow users to query for other users in the calendar route

--- controllers/calendar_controller.py
+++ controllers/calendar_controller.py
@@ -1,5 +1,6 @@
 from fastapi import Request, HTTPException
 from fastapi.responses import JSONResponse
+from models.user import User
 import logging
 
 logger = logging.getLogger(__name__)
@@ -34,6 +35,65 @@ async def fetch_calendar_app_data(request: Request):
         return JSONResponse(
             content={
                 'detail': 'There was an issue processing your request',
+            },
+            status_code=500
+        )
+    
+async def fetch_users_query(request: Request):
+    try: 
+        user_query = request.query_params.get('user', default=None)
+        
+        if user_query:
+            # cursor for querying the db
+            cursor = request.app.db['users'].find(
+                {"$text": {"$search": user_query}},
+                {"score": {"$meta": "textScore"}}
+            ).sort([("score", {"$meta": "textScore"})])
+
+            # store non-sensitive data of users and async loop cursor through db
+            user_search_results = []
+            async for document in cursor:
+                user_ref = {
+                    "company": document["company"] if document["company"] else '',
+                    "email": document["email"] if document["email"] else '',
+                    "first_name": document["first_name"] if document["first_name"] else '',
+                    "job_title": document["job_title"] if document["job_title"] else '',
+                    "last_name": document["last_name"] if document["last_name"] else '',
+                }
+                user_search_results.append({"user": user_ref, "score": document.pop("score")})
+
+
+            if user_search_results is not None:
+                # convert the user results to dict
+                return JSONResponse(
+                    content={
+                        'detail': 'Results found',
+                        'user_Results': user_search_results,
+                    },
+                    status_code=200
+                )
+
+            else:
+                return JSONResponse(
+                    content={
+                        'detail': 'There were no matches for that query',
+                    },
+                    status_code=404
+                )
+
+        else:
+            return JSONResponse(
+                content={
+                    'detail': 'No userQuery parameter to lookup in db',
+                },
+                status_code=422
+            )
+
+    except Exception as e:
+        logger.error(f"Error processing request: {e}")
+        return JSONResponse(
+            content={
+                'detail': 'There was an issue processing your request'
             },
             status_code=500
         )
--- routes/calendar_routes.py
+++ routes/calendar_routes.py
@@ -4,8 +4,10 @@ from scripts.jwt_token_decoders import process_bearer_token
 
 calendar_router = APIRouter()
 
-# all calendar routes go here
 @calendar_router.get('/')
 async def get_calendar_data(request: Request, token: str | bool = Depends(process_bearer_token)):
-    print(request, token)
     return await calendar_controller.fetch_calendar_app_data(request)
+
+@calendar_router.get('/userQuery')
+async def get_user_query(request: Request, token: str | bool = Depends(process_bearer_token)):
+    return await calendar_controller.fetch_users_query(request)
