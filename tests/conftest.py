e672a19d4c42bde82518c491fe72bd92cadd9028
DutsAndrew <dutsandrew@gmail.com>
2024-02-09 12:22:11 -0700

    updated test suite, calendar app data fetch fully tested, added test_token conftest function

--- tests/conftest.py
+++ tests/conftest.py
@@ -5,11 +5,14 @@ from fastapi.testclient import TestClient
 from motor.motor_asyncio import AsyncIOMotorClient
 from main import app as main_app
 from dotenv import dotenv_values
+from scripts.jwt_token_encoders import encode_bearer_token
+from models.user import UserLogin
 
 @pytest.fixture
 def test_client():
     app = TestClient(main_app)
     return app
+
 
 @pytest.fixture
 def test_client_with_db():
@@ -26,6 +29,15 @@ def test_client_with_db():
     app = TestClient(main_app)
 
     return app
+
+
+@pytest.fixture(scope="session")
+def generate_test_token():
+    user_login = UserLogin(email="test@gmail.com", password="Test123$")
+
+    bearer_token = encode_bearer_token(user_login=user_login)
+
+    return bearer_token
 
 
 # if async operations are needed use:
--- tests/test_calendar_routes.py
+++ tests/test_calendar_routes.py
@@ -1,26 +1,5 @@
 import pytest
-from controllers.calendar_controller import fetch_calendar_app_data
+from fastapi.testclient import TestClient
 
-def test_fetch_calendar_app_data(test_client_with_db):
-
-    response = test_client_with_db.get(
-        '/calendar/',
-        headers={'content-type': 'application/json'}
-    )
-
-    assert response.status_code == 200
-    assert response.json() == {
-        'detail': 'Calendar Data Loaded',
-        'data': {
-            '_id': str,
-            "app_data_type": "calendar",
-            "calendar_dates": {
-                "2023": {
-                    "January": {
-                        "days": 31,
-                        "month_starts_on": "Thursday",
-                    }
-                }
-            }
-        }
-    }
+# For when wanting to isolate tests:
+# @pytest.mark.skip(reason='Not implemented')
